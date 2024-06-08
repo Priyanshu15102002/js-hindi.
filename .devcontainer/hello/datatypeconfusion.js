6aa3f54b412257759a7e8f3837df6115bef341f5
Priyanshu15102002 <134455533+Priyanshu15102002@users.noreply.github.com>
2024-06-08 21:20:50 +0000

    conversion of data types

---
+++ .devcontainer/hello/datatypeconfusion.js
@@ -0,0 +1,17 @@
+let score="33"
+console.log(typeof score)
+let score_in_number=Number(score)
+console.log(score_in_number)
+console.log(typeof score_in_number)
+//"33"=>33
+//"33abc"=>Nan
+//true=>1;false=0
+let isLogged=1;
+let isLoggenbool=Boolean(isLogged);
+console.log(isLoggenbool)
+//1=>true
+//""=false
+//"hello"=true
+let s=5;
+let strings=String(s)
+console.log(typeof strings)
