f91712902aa37186f9532e88aab60d2e4d04d49d
Priyanshu15102002 <134455533+Priyanshu15102002@users.noreply.github.com>
2024-06-08 20:50:38 +0000

    second commit

---
+++ .devcontainer/hello/datatypes.js
@@ -0,0 +1,14 @@
+"use strict";
+let name="priyanshu"
+let age
+let ishappy=true
+let state=null
+//number 2power 53
+//bigint
+//string=""
+//null=>standalone value
+//undefined=undefine
+//symbol=>uniqueness
+//object
+console.log(typeof age) //undefined
+console.log(typeof state) //object
---
+++ .devcontainer/hello/variables.js
@@ -0,0 +1,9 @@
+const accountId=247394;
+let accountEmail="abc@gmail.com"
+var accountName="priyanshu"
+accountCity="Agra"
+accountEmail="dsc@gmail.com"
+let accountstate;
+//accountId=2;
+//dont use var because of block scope and functional scope issues
+console.table([accountId,accountName,accountEmail,accountCity,accountstate])
